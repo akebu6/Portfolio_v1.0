6d8cf71f878bdecdca579a58dd586ae990bd83ba
akebu6 <asimasiku999@gmail.com>
2024-02-12 18:09:38 +0200

    Initial Commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const LINKS = {
+  github: "https://github.com/akebu6",
+  twitter: "https://twitter.com/akebu6",
+  linkedin: "https://www.linkedin.com/in/akebu-simasiku-24186720a/",
+  hashnode: "https://akebu6.hashnode.dev/",
+  discord: "https://discordapp.com/users/akebu",
+  devpost: "https://devpost.com/akebu6"
+}
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
